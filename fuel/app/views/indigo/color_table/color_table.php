6a9e3ef048e08c46332d3b3477af6947fb2ef0de
quick-111 <connorquick2112@gmail.com>
2022-03-29 13:08:38 -0600

    hey

--- fuel/app/views/indigo/color_table/color_table.php
+++ fuel/app/views/indigo/color_table/color_table.php
@@ -1,5 +1,10 @@
 <body>
     <section>
         This is the page that will contain the color table
+        <form action="color_table.php" method="post">
+        <p>Number of Rows: <input type="number" name="rows" /></p>
+        <p>Number of Columns: <input type="number" name="columns" /></p>
+        <p><input type="submit" /></p>
+</form>
     </section>
 </body>
