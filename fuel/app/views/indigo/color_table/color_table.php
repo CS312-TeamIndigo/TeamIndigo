2b7b2587eba893fe48ebbc248ebd3cdbdf568cc0
quick-111 <connorquick2112@gmail.com>
2022-03-29 13:37:43 -0600

    if/else

--- fuel/app/views/indigo/color_table/color_table.php
+++ fuel/app/views/indigo/color_table/color_table.php
@@ -1,13 +1,15 @@
 <body>
     <section>
         This is the page that will contain the color table
-        <form action="color_table.php" method="post">
+        <form action="color_table.php" method="get">
         <p>Number of Rows: <input type="number" name="rows" min='1' max='26'/></p>
         <p>Number of Columns: <input type="number" name="columns" min='1' max='10' /></p>
         <p><input type="submit" /></p>
 
-        <p>Rows: <?php if($_POST["rows"]){ echo $_POST["rows"]; } ?><br></p>
-        <p>Columns: <?php if($_POST["columns"]){ echo $_POST["columns"]; } ?></p>
+        <p>Rows: <?php if($_GET["rows"]){ echo $_GET[["rows"]; } 
+        else{ echo "none"} ?><br></p>
+        <p>Columns: <?php if($_GET["columns"]){ echo $_GET["columns"]; } 
+        else{ echo "none"}?></p>
 
 </form>
     </section>
