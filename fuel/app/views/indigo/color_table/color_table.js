458ddf2ca3212108bf9654fba1512da743604340
quick-111 <connorquick2112@gmail.com>
2022-03-30 12:09:48 -0600

    there

--- fuel/app/views/indigo/color_table/color_table.js
+++ fuel/app/views/indigo/color_table/color_table.js
@@ -1,4 +1,4 @@
-$("#colorList").change(() => {
+$(".colorList").change(() => {
     $color_index = this.value;
     console.log("it is being called");
     // add the center-header class to #part1 (the h3 tag)
