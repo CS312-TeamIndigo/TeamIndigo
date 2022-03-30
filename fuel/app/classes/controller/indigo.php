80c1ac547524e340a3ce03a819373fff23d2e9d1
quick-111 <connorquick2112@gmail.com>
2022-03-29 20:23:02 -0600

    there

--- fuel/app/classes/controller/indigo.php
+++ fuel/app/classes/controller/indigo.php
@@ -31,11 +31,35 @@ Class Controller_Indigo extends Controller_Template{
     public function action_color_table() {
     
         $data = array();
-        $this->template->rows = Input::post('rows');
-        $this->template->colors = Input::post('colors');
         $this->template->title= 'Color Table';
         $this->template->content = View::forge('indigo/color_table/color_table',$data);
         $this->template->css = 'east.css';
         $this->template->filename = 'indigo.png';
 	}
+
+
+    public function post_color_table() {
+        $rows = Input::post('rows');
+        $colors = Input::post('colors');
+        if ($rows && $colors) {
+            FuelFormExample::log_message($rows, $colors);
+            $success_view = View::forge("indigo/color_table/success.php");
+            $data = array(
+                "success_view" => $success_view
+            );
+            return Response::forge(View::forge("indigo/color_table/color_table", $data));
+        } else {
+            $fail_view = View::forge("indigo/color_table/failure.php");
+            $data = array(
+                "failure_view" => $fail_view
+            );
+            return Response::forge(View::forge("indigo/color_table/color_table", $data));
+        }
+        $data = array();
+        $this->template->title= 'Color Table';
+        $this->template->content = View::forge('indigo/color_table/color_table',$data);
+        $this->template->css = 'east.css';
+        $this->template->filename = 'indigo.png';
+	}
+    
 }
