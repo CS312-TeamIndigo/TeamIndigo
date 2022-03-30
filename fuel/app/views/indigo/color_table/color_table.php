9dc49e768595f4e0a315409c06186329f1ad4e58
quick-111 <connorquick2112@gmail.com>
2022-03-30 09:30:23 -0600

    there

--- fuel/app/views/indigo/color_table/color_table.php
+++ fuel/app/views/indigo/color_table/color_table.php
@@ -1,9 +1,9 @@
 <body>
     <section>
-        This is the page that will contain the color table
+        Please Enter The Number of Rows and the Number of Colors:
         <form action="color_table" method="post">
         <p>Number of Rows: <input type="number" name="rows" id="rows" min='1' max='26'/></p>
-        <p>Number of Columns: <input type="number" name="colors" id="colors" min='1' max='10' /></p>
+        <p>Number of Colors: <input type="number" name="colors" id="colors" min='1' max='10' /></p>
         <p><input type="submit" /></p>
 
         <?php
--- fuel/app/views/indigo/color_table/failure.php
+++ fuel/app/views/indigo/color_table/failure.php
@@ -1,3 +1,23 @@
-<p>
-  Your form submission has FAILED :(
-</p>
+<body>
+<section>
+    <h3>Please Enter A Vaild Number!</h3><br>
+    Please Enter The Number of Rows and the Number of Colors:
+    <form action="color_table" method="post">
+        <p>Number of Rows: <input type="number" name="rows" id="rows" min='1' max='26' /></p>
+        <p>Number of Colors: <input type="number" name="colors" id="colors" min='1' max='10' /></p>
+        <p><input type="submit" /></p>
+
+        <?php
+
+        echo Form::open(array(
+            "action" => "index/indigo",
+            "method" => "post",
+            "id" => "fuel_form"
+        ));
+
+        echo Form::close();
+        ?>
+
+    </form>
+</section>
+</body>
