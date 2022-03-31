45b22bc6a595906e3757e0353192758042d8b50b
quick-111 <connorquick2112@gmail.com>
2022-03-30 21:06:04 -0600

    solving

--- fuel/app/views/indigo/index.php
+++ fuel/app/views/indigo/index.php
@@ -1,5 +1,6 @@
-<body>
-    <section>
-        <p>Welcome to Team Indigo</p>
-    </section>
-</body>
+<div class="center">
+<p>Welcome to Team Indigo</p>
+<div class="imageMain">
+    <img><?php echo Asset::img("small.png") ?>
+</div>
+</div>
