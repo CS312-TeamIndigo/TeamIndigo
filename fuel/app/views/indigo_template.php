993f02271c5c5f8d378f4dc7129eb96f20505ee8
quick-111 <connorquick2112@gmail.com>
2022-03-30 20:48:08 -0600

    down

--- fuel/app/views/indigo_template.php
+++ fuel/app/views/indigo_template.php
@@ -1,3 +1,5 @@
+es (69 sloc)  2.33 KB
+   
 <head>
     <title>
         <?php echo $title ?>
@@ -8,33 +10,67 @@
     <meta name="author" content="Team Indigo">
     <meta name="keywords" content="Indigo, Team Indigo, color, colors, indigo">
 
-    <!-- <?php echo Asset::css($css) ?> putting this here has no effect -->
     <?php echo Asset::css($css) ?>
 </head>
-<div id="wrapper">
-    <body>
-        <header>
-            <br/>
-            <div class="nav">
-                <a href="index">Home</a>
-                <a href="about">About</a>
-                <a href="color_table">Color Table</a>
-            </div>
-            <br/>
-            <img class="center">
-            <?php echo  Asset::img($filename) ?> 
-            </img>
-        </header>
-        <section>
-            <h1><?php echo $title?></h1>
-            <?php echo $content ?>
-        </section>
-        <footer>
-            <br />
-            <br />
-            <br />
-            <br />
-            Author: Team Indigo -------- Last Modified: 3/26/2022
-       </footer>
-    </body>
-</div>
+<body>
+<header>
+    <div class="center">
+
+
+            <?php echo Asset::img($smallPic) ?>
+        <div class="grid-container">
+        <div class="grid-child right">
+            <h1><?php echo $headerTitle; ?> </h1>
+        </div>
+    </div>
+    </div>
+</header>
+<nav>
+    <ul class="navLink">
+        <a href="index"><span class="spanNav">Home</span></a></li>  |
+        <a href="about"><span class="spanNav">About</span></a></li> |
+        <a href="color_table"><span class="spanNav">ColorTable</span></a></li>
+    </ul>
+</nav>
+
+<br/>
+<?php echo $content; ?>
+</body>
+
+<footer>
+    <?php $thisPage = 'footer'; ?>
+    <div class="footerSpan"></div>
+    <br>
+    <div class="footerLinks">
+        <a href="index">Home</a> -
+        <a href="about">About</a> -
+        <a href="color_table">ColorTable</a>
+    </div>
+    <br>
+    <div class="footerSocial">
+        <a href="#"
+           target="_blank"
+           onClick="return confirm('WARNING! You are now leaving this site. Would you like to continue?')">Linkedin</a>
+        |
+        <a href="#"
+           target="_blank"
+           onClick="return confirm('WARNING! You are now leaving this site. Would you like to continue?')">Facebook</a>
+        |
+        <a href="#" target="_blank"
+           onClick="return confirm('WARNING! You are now leaving this site. Would you like to continue?')">Twitch</a>
+        |
+        <a href="#" target="_blank"
+           onClick="return confirm('WARNING! You are now leaving this site. Would you like to continue?')">Github</a>
+        |
+        <a href="#"
+           target="_blank"
+           onClick="return confirm('WARNING! You are now leaving this site. Would you like to continue?')">Steam</a>
+    </div>
+    <br>
+    <div>
+        Team Indigo | <a class="footerLink" href="#">TeamIndigo@gmail.com</a> |
+        Fort Collins, Colorado
+        <br>&copy; Copyrighted 2022 Team Indigo | Colorado State University Project
+    </div>
+    <br>
+</footer>
