9b1190ce161eb96bd7dd12d406215d842b619a8c
mcmattson <83243547+mcmattson@users.noreply.github.com>
2022-04-19 10:43:06 -0600

    Update about.php

--- fuel/app/views/indigo/about.php
+++ fuel/app/views/indigo/about.php
@@ -1,33 +1,34 @@
+<div class="sectionSpanSubTitle"></div>
 <div class="center">
     <section>
         <div class="grid-container">
-
             <div class="grid-child left">
                 <div class="left">
-                <img>
-                <?php echo Asset::img("connor.png") ?>
-            </div>
+                    <img>
+                    <?php echo Asset::img("connor.png") ?>
+                </div>
             </div>
             <div class="left">
-            <div class="grid-child right">
-                <ul><h2> Connor Quick </h2>
-                    <li> Computer Science Major with a focus on Networks and Security</li>
-                    <li> Aspiring towards Cybersecurity and Software Development careers</li>
-                    <li> Carnegie Hero Award Recipient</li>
-                </ul>
+                <div class="grid-child right">
+                    <ul><h2> Connor Quick </h2>
+                        <h4 style="margin-top: -15px; margin-bottom: -1px">Lead Engineer</h4>
+                        <li> Computer Science Major with a focus on Networks and Security</li>
+                        <li> Aspiring towards Cybersecurity and Software Development careers</li>
+                        <li> Carnegie Hero Award Recipient</li>
+                    </ul>
+                </div>
             </div>
-            </div>
+            <div class="sectionSpan"></div>
         </div>
     </section>
-    <br>
     <section>
-
         <div class="grid-container">
             <div class="left">
                 <div class="grid-child left">
                     <ul><h2> Matthew Mattson </h2>
+                        <h4 style="margin-top: -15px; margin-bottom: -1px">Front End Engineer</h4>
                         <li> Computer Science Major with an open outlook on specializations</li>
-                        <li> Father of Two</li>
+                        <li> Father of Two</li> <br>
                         <li> Knowledgeable and Compassionate Leader</li>
                         <li> Rapidly Expanding Knowledge of Programming Languages, Workflow Programs and Problem
                             Solving
@@ -38,9 +39,9 @@
             <div class="grid-child right">
                 <img><?php echo Asset::img("matt.png") ?>
             </div>
+            <div class="sectionSpan"></div>
         </div>
     </section>
-    <br>
     <section>
         <div class="grid-container">
             <div class="grid-child left">
@@ -48,23 +49,23 @@
             </div>
 
             <div class="left">
-            <div class="grid-child right">
-                <ul><h2> Max Fink </h2>
-                    <li> Journalism and Media Communication Major with a Minor in Computer Science, focus on
-                        Networks and
-                        Security
-                    </li>
-                    <li> Work as a Broadcast Engineer</li>
-                    <li> Building Experience to work on TV trucks within the sports video industry</li>
-                    <li> Experiences working in CU Athletics- BuffVision and EIC for the PAC12 network, Lead AV for
-                        the
-                        Budweiser
-                        Event Center in Loveland, and Senior Media Specialist for CSU athletics- RamVision
-                    </li>
-                </ul>
-            </div>
+                <div class="grid-child right">
+                    <ul><h2> Max Fink </h2>
+                        <h4 style="margin-top: -15px; margin-bottom: -1px">Back End Engineer</h4>
+                        <li> Journalism and Media Communication Major with a Minor in Computer Science, focus on
+                            Networks and
+                            Security
+                        </li>
+                        <li> Work as a Broadcast Engineer</li>
+                        <li> Building Experience to work on TV trucks within the sports video industry</li>
+                        <li> Experiences working in CU Athletics- BuffVision and EIC for the PAC12 network, Lead AV for
+                            the
+                            Budweiser
+                            Event Center in Loveland, and Senior Media Specialist for CSU athletics- RamVision
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </section>
-
 </div>
